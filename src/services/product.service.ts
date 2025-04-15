a9c4bb37963cd1884b44beef0261d7d11943b0d0
alghozii_zyy <shobarudin67@students.amikom.ac.id>
2025-04-15 18:54:04 +0700

    update-productService

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -2,7 +2,11 @@ import { db } from '../config/db';
 import { products } from '../models/schema';
 import { eq } from 'drizzle-orm';
 
-export const createProduct = async (product: { name: string; description?: string; price: number }) => {
+export const createProduct = async (
+  product: { 
+  name: string; 
+  description?: string;
+  price: number }) => {
   const [newProduct] = await db.insert(products).values(product).returning();
   return newProduct;
 };
@@ -11,7 +15,12 @@ export const getProducts = async () => {
   return await db.select().from(products);
 };
 
-export const updateProduct = async (id: number, product: Partial<{ name: string; description?: string; price: number }>) => {
+export const updateProduct = async (
+  id: number, 
+  product: Partial<{ 
+    name: string;
+    description?: string; 
+    price: number }>) => {
   const [updatedProduct] = await db.update(products).set(product).where(eq(products.id, id)).returning();
   return updatedProduct;
 };
